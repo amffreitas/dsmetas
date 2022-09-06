e08bed33fdd74981c260adb16400085f863bd8c9
andrefreitas <andre.freitas@esig.com.br>
2022-09-06 19:43:33 -0300

    DatePicker

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,12 +1,12 @@
-import icon from '../../assets/img/notification-icon.svg'
-import './style.css'
+import icon from "../../assets/img/notification-icon.svg";
+import "./style.css";
 
 function NotificationButton() {
-    return (
-        <div className="dsmeta-red-btn">
-            <img src={icon} alt="Notificar" />
-        </div>
-    )
-  }
-  
-  export default NotificationButton
+  return (
+    <div className="dsmeta-red-btn">
+      <img src={icon} alt="Notificar" />
+    </div>
+  );
+}
+
+export default NotificationButton;
